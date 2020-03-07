018ba77adbbd8796a231e2df22d26d3ca34d9669
Iratuã Júnior <iratuajunior@MacBook-Pro-de-Iratua.local>
2020-03-07 17:28:48 -0300

    Pull original

---
+++ FULLSTACK-SPECIALIZATION/front-end-react/dishesapp/src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import { Navbar, NavbarBrand } from 'reactstrap';
+
+import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+  render(){
+    return(
+      <Menu dishes={this.state.dishes} />
+    );
+  }
+}
+
+export default App;
