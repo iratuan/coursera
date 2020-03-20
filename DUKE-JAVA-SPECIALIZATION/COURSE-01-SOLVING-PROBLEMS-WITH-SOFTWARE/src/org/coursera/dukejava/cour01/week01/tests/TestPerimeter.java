ac570143c47183578526f86f27f5e140c9f462a1
Iratuã Júnior <iratuan@gmail.com>
2020-03-20 19:28:24 -0300

    DUKE JAVA 01 - WEEK 01 - Resolucao 1 semana

--- DUKE-JAVA-SPECIALIZATION/COURSE-01-SOLVING-PROBLEMS-WITH-SOFTWARE/src/org/coursera/dukejava/cour01/week01/tests/TestPerimeter.java
+++ DUKE-JAVA-SPECIALIZATION/COURSE-01-SOLVING-PROBLEMS-WITH-SOFTWARE/src/org/coursera/dukejava/cour01/week01/tests/TestPerimeter.java
@@ -10,12 +10,13 @@ import org.junit.jupiter.api.Test;
 
 class TestPerimeter {
 	DecimalFormat formatter = new DecimalFormat("#.##");
-	String path1 = "//Volumes//Dados//Estudos//Coursera//DUKE-JAVA-SPECIALIZATION//COURSE-01-SOLVING-PROBLEMS-WITH-SOFTWARE/data/datatest1.txt";
-	String path2 = "//Volumes//Dados//Estudos//Coursera//DUKE-JAVA-SPECIALIZATION//COURSE-01-SOLVING-PROBLEMS-WITH-SOFTWARE/data/datatest2.txt";
-	String path3 = "//Volumes//Dados//Estudos//Coursera//DUKE-JAVA-SPECIALIZATION//COURSE-01-SOLVING-PROBLEMS-WITH-SOFTWARE/data/datatest3.txt";
-	String path4 = "//Volumes//Dados//Estudos//Coursera//DUKE-JAVA-SPECIALIZATION//COURSE-01-SOLVING-PROBLEMS-WITH-SOFTWARE/data/datatest4.txt";
-	String path5 = "//Volumes//Dados//Estudos//Coursera//DUKE-JAVA-SPECIALIZATION//COURSE-01-SOLVING-PROBLEMS-WITH-SOFTWARE/data/datatest5.txt";
-	String path6 = "//Volumes//Dados//Estudos//Coursera//DUKE-JAVA-SPECIALIZATION//COURSE-01-SOLVING-PROBLEMS-WITH-SOFTWARE/data/datatest6.txt";
+	String pathSystem ="//Volumes//Dados//Estudos//Coursera//DUKE-JAVA-SPECIALIZATION//COURSE-01-SOLVING-PROBLEMS-WITH-SOFTWARE/data/";
+	String path1 = pathSystem + "datatest1.txt";
+	String path2 = pathSystem + "datatest2.txt";
+	String path3 = pathSystem + "datatest3.txt";
+	String path4 = pathSystem + "datatest4.txt";
+	String path5 = pathSystem + "datatest5.txt";
+	String path6 = pathSystem + "datatest6.txt";
 	Shape s1 =  new Shape(path1);
 	Shape s2 =  new Shape(path2);
 	Shape s3 =  new Shape(path3);
