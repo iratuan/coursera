128a1cb4e2842f8436449023c80310d6720ece45
Iratuã Júnior <iratuan@gmail.com>
2020-05-03 22:05:18 -0300

    Resolução do primeiro desafio da semana 03

---
+++ DUKE-JAVA-SPECIALIZATION/COURSE-01-SOLVING-PROBLEMS-WITH-SOFTWARE/WEEK03/WhichCountriesExport.java
@@ -0,0 +1,48 @@
+/**
+ * Reads a chosen CSV file of country exports and prints each country that exports coffee.
+ * 
+ * @author Duke Software Team 
+ */
+import edu.duke.*;
+import org.apache.commons.csv.*;
+public class WhichCountriesExport {
+    
+    public void listExportersByEconomy(CSVParser parser) {
+        //for each row in the CSV File
+        for(CSVRecord record : parser){                  
+            String value = record.get("Value (dollars)");       
+            if(value.length() > 17){
+                System.out.println("País: " + record.get("Country") + ", Valor: " + record.get("Value (dollars)"));
+            }
+            
+        }
+        
+
+    }
+    public void listExporters(CSVParser parser, String exportOfInterest) {
+        //for each row in the CSV File
+        for(CSVRecord record : parser){                  
+            String export = record.get("Exports");
+            String[] params = exportOfInterest.split(",");
+            int count = 0;
+            for(String p : params){
+                if(export.indexOf(p) != -1){
+                        count ++;
+                }
+            }
+            
+            if(count == params.length){
+                System.out.println(record.get("Country"));
+            }
+            
+        }
+        
+
+    }
+
+    public void whoExports() {
+        FileResource fr = new FileResource();
+        CSVParser parser = fr.getCSVParser();
+        listExportersByEconomy(parser);
+    }
+}
